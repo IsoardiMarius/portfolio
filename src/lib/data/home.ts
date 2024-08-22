0a946f15bd23ff025c68ff30c394b6594507748f
IsoardIMarius <misoardi13100@outlook.fr>
2024-08-22 02:56:37 +0200

    init

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -15,7 +15,7 @@ export const description =
 	'Développeur et expert DevOps, je conçois des solutions digitales sur mesure qui allient innovation et performance. Disponible pour donner vie à vos projets freelance dès 250€ par jour. Construisons ensemble l\'avenir de votre entreprise.'
 
 export const subDescription =
-	'Vous avez ci-dessous différents liens, comme mon Github, Linkedin, mon mail, etc. Pour un premier contact n\'hésitez pas à me contact idéalement par Linkedin ou email.'
+	'Vous avez ci-dessous différents liens, comme mon Github, Linkedin, mon mail, etc. Pour un premier contact n\'hésitez pas à me contact idéalement par Linkedin / email.'
 export const links: Array<{ platform: Platform; link: string }> = [
 	{ platform: Platform.GitHub, link: 'https://github.com/IsoardiMarius' },
 	{
